9fdb2223ab8726688901571a8def427231ee7b1e
David Gadling <dave@toasterwaffles.com>
2009-05-12 14:14:47 -0700

    update.sh: Update solar data as well.

--- update.sh
+++ update.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+export PYTHONPATH=$PYTHONPATH:/home/httpd/traffic.toasterwaffles.com
 vars="ifInOctets.4 ifOutOctets.4"
 
 eval `snmpget -O Qs -v 1 -c public 192.168.1.1 $vars | sed -e 's/\.[0-9] = /=/'`
 
 rrdtool update /home/dave/traffic.toasterwaffles.com/myrouter.rrd N:${ifInOctets}:${ifOutOctets}
 rrdtool update /home/dave/traffic.toasterwaffles.com/myrouter.rrd.bak N:${ifInOctets}:${ifOutOctets}
+/home/dave/traffic.toasterwaffles.com/fetch-solar.py
