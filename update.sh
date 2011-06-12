4d2769c5644b1d89dc490f4c71ed31b56b25ad24
David Gadling <dave@toasterwaffles.com>
2011-06-11 18:35:21 -0700

    Don't need to write to the backup file, derp.

--- update.sh
+++ update.sh
@@ -6,5 +6,4 @@ vars="ifInOctets.4 ifOutOctets.4"
 eval `snmpget -O Qs -v 1 -c public 192.168.1.1 $vars | sed -e 's/\.[0-9] = /=/'`
 
 rrdtool update /home/dave/traffic.toasterwaffles.com/myrouter.rrd N:${ifInOctets}:${ifOutOctets}
-rrdtool update /home/dave/traffic.toasterwaffles.com/myrouter.rrd.bak N:${ifInOctets}:${ifOutOctets}
 /home/dave/traffic.toasterwaffles.com/fetch-solar.py
